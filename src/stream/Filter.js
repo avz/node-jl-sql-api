ec6f49ba8bd3df2908da8ce925a1712b0b5505cd
Artem Zaytsev <arepo@nologin.ru>
2016-10-29 20:43:08 +0300

    Filter stream

---
+++ src/stream/Filter.js
@@ -0,0 +1,30 @@
+const JlTransform = require('./JlTransform');
+
+class Filter extends JlTransform
+{
+	constructor(filterCb)
+	{
+		super(JlTransform.ARRAYS_OF_OBJECTS, JlTransform.ARRAYS_OF_OBJECTS);
+
+		this.filter = filterCb;
+	}
+
+	_transform(chunk, encoding, cb)
+	{
+		var result = [];
+
+		for (let i = 0; i < chunk.length; i++) {
+			if (this.filter(chunk[i])) {
+				result.push(chunk[i]);
+			}
+		}
+
+		if (result.length) {
+			this.push(result);
+		}
+
+		cb();
+	}
+}
+
+module.exports = Filter;
