611db7e2e7e5cc2b87fdf7cfdc7ce9388acfd9c7
Artem Zaytsev <avz@nologin.ru>
2016-11-06 12:53:32 +0300

    Remove unused checking

--- src/stream/ChunkJoiner.js
+++ src/stream/ChunkJoiner.js
@@ -1,7 +1,34 @@
 const JlTransform = require('./JlTransform');
 
-class ChunkJoiner extends JlTransform.Objects_To_ArraysOfObjects
+class ChunkJoiner extends JlTransform
 {
+	constructor(chunkMaxSize = 100)
+	{
+		super(JlTransform.OBJECTS, JlTransform.ARRAYS_OF_OBJECTS);
+
+		this.chunkMaxSize = chunkMaxSize;
+		this.chunk = [];
+	}
+
+	_transform(object, charset, cb)
+	{
+		this.chunk.push(object);
+
+		if (this.chunk.length >= this.chunkMaxSize) {
+			this.push(this.chunk);
+			this.chunk = [];
+		}
+
+		cb();
+	}
+
+	_flush(cb)
+	{
+		this.push(this.chunk);
+		this.chunk = [];
+
+		cb();
+	}
 }
 
 module.exports = ChunkJoiner;
--- src/stream/ChunkSplitter.js
+++ src/stream/ChunkSplitter.js
@@ -1,7 +1,20 @@
 const JlTransform = require('./JlTransform');
 
-class ChunkSplitter extends JlTransform.ArraysOfObjects_To_Objects
+class ChunkSplitter extends JlTransform
 {
+	constructor()
+	{
+		super(JlTransform.ARRAYS_OF_OBJECTS, JlTransform.OBJECTS);
+	}
+
+	_transform(chunk, charset, cb)
+	{
+		for (var i = 0; i < chunk.length; i++) {
+			this.push(chunk[i]);
+		}
+
+		cb();
+	}
 }
 
 module.exports = ChunkSplitter;
