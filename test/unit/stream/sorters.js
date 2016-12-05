1af65ebce08351ebcc4389670091043eaab3d242
Artem Zaytsev <avz@nologin.ru>
2016-12-05 19:08:25 +0300

    More tests

---
+++ test/unit/stream/sorters.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const SorterInMemory = require('../../../src/stream/SorterInMemory');
+const SorterExternal = require('../../../src/stream/SorterExternal');
+const SortOptions = require('../../../src/SortOptions');
+const Order = require('../../../src/Order');
+
+for (const sorterCtor of [SorterInMemory, SorterExternal]) {
+	describe(sorterCtor.name, () => {
+		const dataRoot = path.join(__dirname, 'sort');
+
+		const createValueFunction = (dataType, field) => {
+			const cb = (row) => {
+				return row[field];
+			};
+
+			cb.dataType = dataType;
+
+			return cb;
+		};
+
+		for (const f of fs.readdirSync(dataRoot)) {
+			const data = require(path.join(__dirname, 'sort', f));
+
+			for (const testcaseName in data.cases) {
+				const testcase = data.cases[testcaseName];
+				const orders = [];
+
+				for (const [field, direction, type] of testcase.orders) {
+					orders.push(new Order(createValueFunction(type, field), direction));
+				}
+
+				it(f + ' / ' + testcaseName, done => {
+					const sorter = new sorterCtor(orders, new SortOptions);
+					let sorted = [];
+
+					sorter.on('end', () => {
+//						for (const r of sorted) {
+//							process.stdout.write(JSON.stringify(r) + ',\n');
+//						}
+						assert.deepStrictEqual(sorted, testcase.expected);
+						done();
+					});
+
+					sorter.on('data', (chunk) => {
+						sorted = sorted.concat(chunk);
+					});
+
+					sorter.end(data.input);
+				});
+			}
+		}
+	});
+}
