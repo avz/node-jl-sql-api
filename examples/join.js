9840586e38dd79eee79251dc75658b159485ae5a
Artem Zaytsev <avz@nologin.ru>
2016-12-16 12:47:38 +0300

    Make eslint happy

--- examples/join.js
+++ examples/join.js
@@ -3,19 +3,19 @@ const JlSqlApi = require('jl-sql-api');
 const api = new JlSqlApi({forceInMemory: true});
 
 api.query('SELECT id AS mid, @child.field INNER JOIN child ON @child.mainId = id')
-    .fromArrayOfObjects([
-        {"id": 1},
-        {"id": 2}
-    ])
-    .addArrayOfObjects('child', [
-        {"mainId": 1, "field": 11},
-        {"mainId": 1, "field": 12},
-        {"mainId": 2, "field": 21},
-        {"mainId": 3, "field": 31}
-    ])
-    .toArrayOfObjects((r) => {
-        console.log(r);
-    })
+	.fromArrayOfObjects([
+		{"id": 1},
+		{"id": 2}
+	])
+	.addArrayOfObjects('child', [
+		{"mainId": 1, "field": 11},
+		{"mainId": 1, "field": 12},
+		{"mainId": 2, "field": 21},
+		{"mainId": 3, "field": 31}
+	])
+	.toArrayOfObjects((r) => {
+		console.log(r);
+	})
 ;
 
 // make runkit.com silent
