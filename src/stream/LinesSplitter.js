eff8f55cc1d7d0852b094a2797018c34e77af530
Artem Zaytsev <avz@nologin.ru>
2017-01-19 11:49:55 +0300

    Fixed: multibyte characters can be broke in some cases

--- src/stream/LinesSplitter.js
+++ src/stream/LinesSplitter.js
@@ -9,25 +9,39 @@ class LinesSplitter extends JlTransform
 		super(JlTransform.RAW, JlTransform.ARRAYS_OF_OBJECTS);
 
 		this.ending = '\n';
-		this.tail = '';
+		this.endingCode = 0x0a;
+
+		this.tail = Buffer.alloc(0);
 	}
 
 	_transform(chunk, encoding, cb)
 	{
-		var lines = ('' + chunk).split(this.ending);
+		const lastLineBreakPos = chunk.lastIndexOf(this.endingCode);
 
-		var bucket = [];
-
-		if (lines.length > 1) {
-			bucket.push(this.tail + lines[0]);
-
-			for (var i = 1; i < lines.length - 1; i++) {
-				bucket.push(lines[i]);
+		if (lastLineBreakPos === -1) {
+			if (this.tail.length) {
+				this.tail = Buffer.concat([this.tail, chunk]);
+			} else {
+				this.tail = chunk;
 			}
 
-			this.tail = lines[lines.length - 1];
-		} else {
-			this.tail += lines[lines.length - 1];
+			cb();
+			return;
+		}
+
+		const chunkWoTail = chunk.slice(0, lastLineBreakPos);
+		const body = Buffer.concat([this.tail, chunkWoTail]).toString();
+
+		this.tail = chunk.slice(lastLineBreakPos + 1);
+
+		const lines = body.split(this.ending);
+
+		const bucket = [];
+
+		for (const line of lines) {
+			if (line.length) {
+				bucket.push(line);
+			}
 		}
 
 		if (bucket.length) {
---
+++ test/unit/stream/LineSplitter.js
@@ -0,0 +1,35 @@
+'use script';
+
+const assert = require('assert');
+const LineSplitter = require('../../../src/stream/LinesSplitter');
+
+describe('LineSplitter', () => {
+	describe('miltibyte', () => {
+		var result = [];
+		const ls = new LineSplitter;
+
+		before((done) => {
+			ls.on('data', (lines) => {
+				result = result.concat(lines);
+			});
+
+			ls.on('end', done);
+
+			ls.write(Buffer.from('hello\nworld\n'));
+			ls.write(Buffer.from([0xd0, 0xbf, 0xd1]));
+			ls.write(Buffer.from([0x80, 0x0a]));
+			ls.write(Buffer.from([0xd0]));
+			ls.write(Buffer.from([0xb8]));
+			ls.write(Buffer.from([0xd0]));
+			ls.write(Buffer.from([0xb2]));
+			ls.write(Buffer.from([0x0a, 0xd0]));
+			ls.write(Buffer.from([0xb5, 0xd1, 0x82, 0x0a, 0xd0, 0xbc, 0xd0, 0xb8, 0xd1, 0x80, 0x0a]));
+
+			ls.end();
+		});
+
+		it('splitted in middle of character', () => {
+			assert.deepStrictEqual(result, ['hello', 'world', 'пр', 'ив', 'ет', 'мир']);
+		});
+	});
+});
